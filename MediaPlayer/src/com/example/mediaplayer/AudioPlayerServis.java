9b181407098e77145336e28305c8fc777622cff2
Satonina Anastasia <satonina.n@gmail.com>
2014-03-23 22:17:20 +0300

    .

---
+++ MediaPlayer/src/com/example/mediaplayer/AudioPlayerServis.java
@@ -0,0 +1,66 @@
+package com.example.mediaplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Binder;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class AudioPlayerServis extends Service implements OnCompletionListener {
+	statusPlayer status = statusPlayer.IDLE;
+	MediaPlayer Player;
+	private final IBinder mBinder = new LocalBinder();
+
+	public class LocalBinder extends Binder {
+		AudioPlayerServis getService() {
+			return AudioPlayerServis.this;
+		}
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return mBinder;
+	}
+
+	public void createPlayer() {
+		Player = MediaPlayer.create(this, R.raw.music);
+		Player.setOnCompletionListener(this);
+	}
+
+	public void pressButtonPlay() {
+
+		switch (status) {
+		case IDLE:
+
+			createPlayer();
+			Player.start();
+			status = statusPlayer.PLAYING;
+
+			break;
+		case PLAYING:
+
+			Player.pause();
+			status = statusPlayer.PAUSED;
+
+			break;
+		case PAUSED:
+
+			Player.start();
+			status = statusPlayer.PLAYING;
+
+			break;
+		}
+	}
+
+	public statusPlayer getStatusPlayer() {
+		return status;
+	}
+
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+		status = statusPlayer.IDLE;
+	}
+
+}
